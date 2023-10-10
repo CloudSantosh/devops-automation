260cd0ad0df5b085f3892f32a407faf8816bcdb1
santosh <santoshtechguy@gmail.com>
2023-10-10 21:19:30 +0300

    first commit

---
+++ src/main/java/com/javatechie/deveopsintergration/DeveopsIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.deveopsintergration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveopsIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveopsIntergrationApplication.class, args);
+	}
+
+}
